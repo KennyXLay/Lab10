e43d98eb3d4246c4198ce6e2e37caf9d01b26864
KennyXLay <62919286+KennyXLay@users.noreply.github.com>
2021-02-24 21:02:56 -0800

    Update main.c

--- source/main.c
+++ source/main.c
@@ -13,17 +13,6 @@
 #include "simAVRHeader.h"
 #endif
 
-#define KEYPADPORT PORTC
-#define KEYPADPIN  PINC
-#define ROW1 0
-#define ROW2 1
-#define ROW3 2
-#define ROW4 3
-#define COL1 4
-#define COL2 5
-#define COL3 6
-#define COL4 7
-
 volatile unsigned char TimerFlag = 0;
 
 unsigned long _avr_timer_M = 1;
@@ -85,7 +74,7 @@ unsigned long int findGCD(unsigned long int a, unsigned long int b){
 	return 0;
 }
 
-/*unsigned char GetKeypadKey(){
+unsigned char GetKeypadKey(){
         PORTC = 0xEF;
         asm("nop");
         if(GetBit(PINC, 0) == 0) { return('1');}
@@ -115,43 +104,8 @@ unsigned long int findGCD(unsigned long int a, unsigned long int b){
         if(GetBit(PINC, 3) == 0) { return('D');}
 
         return ('\0');
-}*/
-
-unsigned char GetKeypadKey(){
-// Check keys in col 1
-	KEYPADPORT = SetBit(0xFF,COL1,0); // Set Px4 to 0; others 1
-	asm("nop"); // add a delay to allow PORTx to stabilize before checking
-	if ( GetBit(~KEYPADPIN,ROW1) ) { return '1'; }
-	if ( GetBit(~KEYPADPIN,ROW2) ) { return '4'; }
-	if ( GetBit(~KEYPADPIN,ROW3) ) { return '7'; }
-	if ( GetBit(~KEYPADPIN,ROW4) ) { return '*'; }
-
-	// Check keys in col 2
-	KEYPADPORT = SetBit(0xFF,COL2,0); // Set Px5 to 0; others 1
-	asm("nop"); // add a delay to allow PORTx to stabilize before checking
-	if ( GetBit(~KEYPADPIN,ROW1) ) { return '2'; }
-	if ( GetBit(~KEYPADPIN,ROW2) ) { return '5'; }
-	if ( GetBit(~KEYPADPIN,ROW3) ) { return '8'; }
-	if ( GetBit(~KEYPADPIN,ROW4) ) { return '0'; }
-
-	// Check keys in col 3
-	KEYPADPORT = SetBit(0xFF,COL3,0); // Set Px6 to 0; others 1
-	asm("nop"); // add a delay to allow PORTx to stabilize before checking
-	if ( GetBit(~KEYPADPIN,ROW1) ) { return '3'; }
-	if ( GetBit(~KEYPADPIN,ROW2) ) { return '6'; }
-	if ( GetBit(~KEYPADPIN,ROW3) ) { return '9'; }
-	if ( GetBit(~KEYPADPIN,ROW4) ) { return '#'; }
-
-	// Check keys in col 4
-	KEYPADPORT = SetBit(0xFF,COL4,0); // Set Px7 to 0; others 1
-	asm("nop"); // add a delay to allow PORTx to stabilize before checking
-	if (GetBit(~KEYPADPIN,ROW1) ) { return 'A'; }
-	if (GetBit(~KEYPADPIN,ROW2) ) { return 'B'; }
-	if (GetBit(~KEYPADPIN,ROW3) ) { return 'C'; }
-	if (GetBit(~KEYPADPIN,ROW4) ) { return 'D'; }
-
-	return '\0';
 }
+
 
 unsigned char keypress = 0x00;
 unsigned char keypad = 0x00;
